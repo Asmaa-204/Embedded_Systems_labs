0fa168f9e393abf9ce5a78bcde1965e198e00749
Asmaa-204 <asmaa.abozaid204@gmail.com>
2025-02-22 08:59:37 +0200

    ex1

---
+++ lab3/ex1/ex1.ino
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile uint8_t overflow = 0;
+
+ISR(TIMER2_OVF_vec) {
+  overflow++;
+}
+
+void timer2_init() { 
+  TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20);
+  TCNT2 = 0; 
+  TIMSK2 = (1 << TOIE2);
+  sei();
+}
+
+void setup() {
+  DDRC |= (1 << PC0);
+  PORTC &= ~(1 << PC0);
+  timer2_init();
+}
+
+void loop() {
+  if(overflow >= 30) {
+    if(TCNT2 >= 132) {
+      PORTC ^= (1 << PC0);
+      TCNT2 = 0;
+      overflow = 0;
+    }
+  }
+}
